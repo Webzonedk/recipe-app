d2d42f8d72e5cb0f152db1d9db1b1e13a4b8a563
Kent Pedersen <webzone@kpdesign.dk>
2021-12-08 09:02:34 +0100

    added ingredient model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-recipes = [];
+recipes: Recipe[] = [
+  new Recipe('Chicken curry', 'A delisious kookedchicken dipped in exclusive curry', 'https://live.staticflickr.com/41/86255926_133c8c4b5a_h.jpg' ),
+  new Recipe('Chicken curry deluxe', 'A delisious kookedchicken dipped in exclusive curry', 'https://live.staticflickr.com/41/86255926_133c8c4b5a_h.jpg' )
+
+];
+
   constructor() { }
 
   ngOnInit(): void {
